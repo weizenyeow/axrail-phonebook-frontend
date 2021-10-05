7ddf402ffd6277ba1f1a578ab2c37217a53bfdee
weizenyeow <weizenyeow@gmail.com>
2021-10-05 17:14:28 +0800

    Add create contact

---
+++ src/components/CreateContact.js
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import { api } from "../services/url";
+
+
+const CreateContact = () => {
+  const [name, setName] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const contact = {name, phoneNumber};
+    console.log(api);
+  }
+
+  return (
+    <div className="create">
+      <h2>Add A New Contact</h2>
+      <form onSubmit={handleSubmit}>
+        <label>Contact Name:</label>
+        <input
+          type="text"
+          required
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <label>Contact Number:</label>
+        <input
+          type="number"
+          required
+          value={phoneNumber}
+          onChange={(e) => setPhoneNumber(e.target.value)}
+        />
+        <button>Add Contact</button>
+      </form>
+    </div>
+  );
+};
+
+export default CreateContact;
