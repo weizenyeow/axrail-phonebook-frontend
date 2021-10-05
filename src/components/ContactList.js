bc09d031046843d02581641b02c7a4a8bd12f97f
weizenyeow <weizenyeow@gmail.com>
2021-10-05 17:14:01 +0800

    Add path from list to details

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -1,15 +1,19 @@
+import { Link } from "react-router-dom";
+
 const ContactList = ({ title, items }) => {
   const renderItems = () => {
     return items.map((item) => (
-      <div className="blog-preview" key={item.id} >
-        <h2>{ item.name }</h2>
-        <p>{ item.phone_number }</p>
+      <div className="contact-preview" key={item.id} >
+        <Link to={`/contacts/${item.id}`}>
+          <h2>{ item.name }</h2>
+          <p>{ item.phone_number }</p>
+        </Link>
       </div>
     ));
   }
 
   return (
-    <div className="blog-list">
+    <div className="contact-list">
       <h2>{ title }</h2>
       {renderItems()}
     </div>
